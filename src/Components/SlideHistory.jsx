bacc6e511166bc06eae85a4c45f16fdaeaa70a8f
JudisP <judisson429@hotmail.com>
2023-10-17 17:35:52 -0300

    Feat(DrLuis) - Estrutura da Página

---
+++ src/Components/SlideHistory.jsx
@@ -0,0 +1,60 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+import { BsStarFill } from "react-icons/bs";
+import { Row } from "reactstrap";
+
+const SlideHistory = () => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      breakpoints={{
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <Row className="estrelas">
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+        </Row>
+        <span>
+          Já era muito satisfeita com a Vascular Clínica, sou atendida pelo Dr.
+          Luis Fernando há bastante tempo. E hoje fiquei mais satisfeita ainda
+          com as novas instalações e equipamentos novos.
+        </span>
+        <span className="paciente">HULDA MARIA</span>
+      </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default SlideHistory;
--- src/Pages/Drluis.jsx
+++ src/Pages/Drluis.jsx
@@ -1,3 +1,11 @@
+import { Container, Row } from "reactstrap";
+import { FaUserMd } from "react-icons/fa";
+import { BsStarFill, BsTelephone, BsWhatsapp } from "react-icons/bs";
+
+import { RxDoubleArrowRight } from "react-icons/rx";
+import SlideHistory from "@/Components/SlideHistory";
+import { LuMail } from "react-icons/lu";
+
 const Drluis = () => {
   return (
     <div className="pagina-drluis">
@@ -5,7 +13,78 @@ const Drluis = () => {
         <img src="https://via.placeholder.com/1300x500.png" />
       </div>
 
-      <h1>Drluis</h1>
+      <Container className="container-drluis">
+        <Row className="apresentacao-drluis">
+          <Row>
+            <FaUserMd />
+          </Row>
+          <Row className="estrelas-drluis">
+            <BsStarFill />
+            <BsStarFill />
+            <BsStarFill />
+            <BsStarFill />
+            <BsStarFill />
+          </Row>
+          <Row className="nome-drluis">
+            <h3>Dr. Luis Fernando de Carvalho Bastos</h3>
+          </Row>
+          <Row className="crm-drluis">
+            <span>Cirurgião Vascular - CRM/DF 11.886</span>
+          </Row>
+        </Row>
+
+        <Row>
+          <div>
+            <h3>FORMAÇÃO, TÍTULOS E CARGOS</h3>
+            <div className="informacoes">
+              <div className="informacao">
+                <span>
+                  <RxDoubleArrowRight />
+                </span>
+                <span>
+                  Formado em medicina pela Escola de Ciências Médicas de
+                  Alagoas.
+                </span>
+              </div>
+            </div>
+          </div>
+        </Row>
+
+        <Row>
+          <div className="titulo-subtitulo">
+            <h3>HISTÓRIAS FELIZES</h3>
+            <span>
+              Confira alguns depoimentos de pacientes atendidos pelo Dr. Luís
+              Fernando Bastos
+            </span>
+          </div>
+
+          <Row>
+            <SlideHistory />
+          </Row>
+
+          <div className="agendamento">
+            <h3>AGENDE AGORA SUA CONSULTA</h3>
+            <div className="contatos">
+              <button className="button button--primary">
+                <a>
+                  <BsWhatsapp /> Whatsapp 
+                </a>
+              </button>
+              <button className="button button--primary">
+                <a>
+                  <BsTelephone /> Telefone 
+                </a>
+              </button>
+              <button className="button button--primary">
+                <a>
+                  <LuMail /> E-mail
+                </a>
+              </button>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </div>
   );
 };
