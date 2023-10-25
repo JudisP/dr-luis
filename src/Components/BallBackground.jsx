1e3e6879c56382da2494722db85a85cec007c76c
JudisP <judisson429@hotmail.com>
2023-10-25 15:08:13 -0300

    Feat(Home) - Componentizando o background de bolinhas

---
+++ src/Components/BallBackground.jsx
@@ -0,0 +1,37 @@
+const BallBackground = () => {
+    return (
+        <div style={{width: '100%', height: '100%', position: 'relative'}}>
+        <div style={{width: 7, height: 7, left: 103.54, top: 248.29, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 104.16, top: 203.29, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 104.78, top: 154.30, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 6, height: 7, left: 105.41, top: 105.30, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 106.02, top: 57.30, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 106.64, top: 8.31, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 102.99, top: 295.28, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 8, left: 68.54, top: 251.84, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 8, left: 69.16, top: 202.85, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 8, left: 69.79, top: 153.85, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 6, height: 8, left: 70.41, top: 104.85, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 8, left: 71.02, top: 56.86, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 8, left: 71.64, top: 7.86, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 8, left: 67.99, top: 294.84, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 34.54, top: 251.41, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 35.17, top: 202.42, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 35.79, top: 153.42, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 6, height: 7, left: 36.41, top: 104.42, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 37.02, top: 56.43, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 37.64, top: 7.43, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 34, top: 294.41, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 0.55, top: 250.98, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 1.17, top: 201.98, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 1.79, top: 152.99, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 6, height: 7, left: 2.41, top: 103.99, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 3.03, top: 56, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 3.65, top: 7, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 0, top: 293.98, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+        <div style={{width: 7, height: 7, left: 0, top: 293.98, position: 'absolute', transform: 'rotate(-89.27deg)', transformOrigin: '0 0', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+    </div>
+    );
+  };
+  
+  export default BallBackground;
---
+++ src/Components/BallBackgroundBig.jsx
@@ -0,0 +1,57 @@
+const BallBackgroundBig = () => {
+    return (
+      <div style={{width: 277, height: 178, position: 'relative'}}>
+      <div style={{width: 5.65, height: 7, left: 34.73, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 74.30, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 113.87, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 4.85, height: 7, left: 153.44, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 192.20, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 231.78, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 271.35, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 0, top: 137, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 34.73, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 74.30, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 113.87, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 4.85, height: 7, left: 153.44, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 192.20, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 231.78, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 271.35, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 0, top: 171, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 34.73, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 74.30, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 113.87, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 4.85, height: 7, left: 153.44, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 192.20, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 231.78, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 271.35, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 0, top: 103, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 34.73, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 74.30, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 113.87, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 4.85, height: 8, left: 153.44, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 192.20, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 231.78, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 271.35, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 8, left: 0, top: 68, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 34.73, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 74.30, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 113.87, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 4.85, height: 7, left: 153.44, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 192.20, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 231.78, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 271.35, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 0, top: 34, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 34.73, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 74.30, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 113.87, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 4.85, height: 7, left: 153.44, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 192.20, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 231.78, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 271.35, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 0, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+      <div style={{width: 5.65, height: 7, left: 0, top: 0, position: 'absolute', background: 'rgba(218, 172, 76, 0.40)', borderRadius: 9999}} />
+    </div>
+    );
+  };
+  
+  export default BallBackgroundBig;
