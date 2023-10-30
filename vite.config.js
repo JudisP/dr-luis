3d742b6ad42d3d633e18dca808ca653574a4f746
JudisP <judisson429@hotmail.com>
2023-10-30 11:09:48 -0300

    Build - Instalação e configuração
    - gitignore
    - package
    - vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import env from '@vitejs/plugin-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), env()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
