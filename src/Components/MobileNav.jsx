00d406243312b41353bc42cedbfcdcf564a13be0
Isac Santos de Sá <isacsants679@gmail.com>
2023-11-24 17:51:33 -0300

    Bugfix(MobileNav)-  correção de erro na estrutura do componente.

--- src/Components/MobileNav.jsx
+++ src/Components/MobileNav.jsx
@@ -5,25 +5,23 @@ import BurgerIcon from "./burgerIcon";
 const MobileNav = ({ isOpen, closeMenu }) => {
   return (
     <div className={`mobile-nav ${isOpen ? "open" : ""}`}>
-      <div className={`mobile-nav ${isOpen ? "open" : ""}`}>
-        <BurgerIcon isOpen={isOpen} toggleMenu={closeMenu} />
+      <BurgerIcon isOpen={isOpen} toggleMenu={closeMenu} />
 
-        <NavLink to="/" onClick={closeMenu}>
-          HOME
-        </NavLink>
-        <NavLink to="/dr-luis" onClick={closeMenu}>
-          DR. LUIS FERNANDO BASTOS
-        </NavLink>
-        <NavLink to="/procedimentos" onClick={closeMenu}>
-          PROCEDIMENTOS
-        </NavLink>
-        <NavLink to="/blog" onClick={closeMenu}>
-          BLOG
-        </NavLink>
-        <NavLink to="/agendamento" onClick={closeMenu}>
-          AGENDAMENTO
-        </NavLink>
-      </div>
+      <NavLink to="/" onClick={closeMenu}>
+        HOME
+      </NavLink>
+      <NavLink to="/dr-luis" onClick={closeMenu}>
+        DR. LUIS FERNANDO BASTOS
+      </NavLink>
+      <NavLink to="/procedimentos" onClick={closeMenu}>
+        PROCEDIMENTOS
+      </NavLink>
+      <NavLink to="/blog" onClick={closeMenu}>
+        BLOG
+      </NavLink>
+      <NavLink to="/agendamento" onClick={closeMenu}>
+        AGENDAMENTO
+      </NavLink>
     </div>
   );
 };
