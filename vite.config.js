7c329953927df927bd04d6e0df7bff3da27cf547
JudisP <judisson429@hotmail.com>
2023-12-07 10:49:08 -0300

    Create main.yml p/ GitHub Pages and Config Vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/dr-luis/",
   server: {
     host: '0.0.0.0',
     port: 3001,
