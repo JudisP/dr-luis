3dcdf27db9458daec65c5f978c1d315feb311dfd
JudisP <judisson429@hotmail.com>
2023-12-08 09:39:35 -0300

    Refactor - Colocando Rota Base

--- src/Layout/footer/Footer.jsx
+++ src/Layout/footer/Footer.jsx
@@ -17,11 +17,11 @@ const Footer = () => {
                     <div className="cont navegacao">
                         <p>Navegação</p>
                         <nav className="links naveg">
-                            <NavLink className="link" to="/" onClick={() => scrollTop(0, 0)} title="Home">Home</NavLink>
-                            <NavLink className="link" to="/dr-luis" onClick={() => scrollTop(0, 0)} title="Dr. Luiz Fernando">Dr. Luis Fernando</NavLink>
-                            <NavLink className="link" to="/procedimentos" onClick={() => scrollTop(0, 0)} title="Procedimentos">Procedimentos</NavLink>
-                            <NavLink className="link" to="/blog" onClick={() => scrollTop(0, 0)} title="Blog">Blog</NavLink>
-                            <NavLink className="link" to="/agendamento" onClick={() => scrollTop(0, 0)} title="Agendamento">Agendamento</NavLink>
+                            <NavLink className="link" to="/dr-luis/" onClick={() => scrollTop(0, 0)} title="Home">Home</NavLink>
+                            <NavLink className="link" to="/dr-luis/dr-luis-fernando" onClick={() => scrollTop(0, 0)} title="Dr. Luiz Fernando">Dr. Luis Fernando</NavLink>
+                            <NavLink className="link" to="/dr-luis/procedimentos" onClick={() => scrollTop(0, 0)} title="Procedimentos">Procedimentos</NavLink>
+                            <NavLink className="link" to="/dr-luis/blog" onClick={() => scrollTop(0, 0)} title="Blog">Blog</NavLink>
+                            <NavLink className="link" to="/dr-luis/agendamento" onClick={() => scrollTop(0, 0)} title="Agendamento">Agendamento</NavLink>
                         </nav>
                     </div>
 
